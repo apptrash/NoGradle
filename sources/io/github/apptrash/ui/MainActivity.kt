89b4c0a6ae18bc817710e02c5e875560e18969ff
Alexander <al.boychenko@tbank.ru>
2025-06-12 12:23:08 +0300

    Remove unused imports from MainActivity.kt

--- sources/io/github/apptrash/ui/MainActivity.kt
+++ sources/io/github/apptrash/ui/MainActivity.kt
@@ -2,12 +2,6 @@ package io.github.apptrash.ui
 
 import android.os.Bundle
 import android.app.Activity
-import android.widget.TextView
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.view.View
-import android.view.Gravity
-import android.graphics.Color
 import io.github.apptrash.R
 
 class MainActivity : Activity() {
